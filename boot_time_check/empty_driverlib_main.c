3c91ec2b8e78bca421d30f09eb578e7eaa8a4a3c
Varshith1203 <varshith.s@isyrius-engineering.de>
2026-06-18 14:00:00 +0530

    Update empty_driverlib_main.c

--- boot_time_check/empty_driverlib_main.c
+++ boot_time_check/empty_driverlib_main.c
@@ -2,7 +2,7 @@
 #include "driverlib.h"
 #include "device.h"
 #include "sha256.h"
-// KLJKJJKLHKLJHL
+// ABC
 volatile uint32_t startCycle;
 volatile uint32_t bootCycles;
 volatile float bootTime_ms;
